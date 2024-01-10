653fc3ae1b9d80e67ff08a5cd2ecd02b5dc7b38a
vince <vincecole6@hotmail.co.uk>
2024-01-10 21:36:29 +0000

    Digit repetition method

--- src/Services/KeyCodes/KeyCodeService.php
+++ src/Services/KeyCodes/KeyCodeService.php
@@ -33,12 +33,35 @@ class KeyCodeService
         return $reversedKey !== $key;
     }
 
-
-
-    }
-
     public function digitRepititionIsValid($key){
+        /*
+            Per above, this can be achieved like this:
 
+            $result = array_count_values(str_split($string));
+            return max($result)>3;
+
+            However in the spirit of things, here is an alternative:
+         */
+
+
+        /*
+            Regex will match any character with the same text as the most recently matched and is greedy, so will match
+            as many times as possible.
+            Example: 111211:
+            Finds 1, matches 1, matches 1, broken by 2.
+            Finds 2, broken by 1.
+            Finds 1, matches 1, end of string.
+            Result:  [111,11]; (because 2 isn't duplicated in this example)
+        */
+        preg_match_all('/(.)\1+/', $key, $matches);
+        $result = array_count_values(str_split($key));
+
+        /*
+            The result of array_count_values will be the sum of the repeat numbers.  In our case, 1=>5, 2=>1.
+            We can be sure if the largest value is over 3, we have at least one duplicate character over 3.  We don't
+            care if there is more than one! :)
+        */
+        return max($result)>3;
     }
 
     public function digitSequenceIsValid($key){
