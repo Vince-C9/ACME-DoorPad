1ac03e0701efd6b10a61ab11f5ee611f50c11c98
vince <vincecole6@hotmail.co.uk>
2024-01-14 03:15:39 +0000

    Add Config and Routes to Service Provider

--- src/AcmeDoorPadBaseServiceProvider.php
+++ src/AcmeDoorPadBaseServiceProvider.php
@@ -4,13 +4,20 @@
 namespace Vince\AcmeDoorPad;
 
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Config;
 use Vince\AcmeDoorPad\Console\SecurityKeyCommand;
 
 class AcmeDoorPadBaseServiceProvider extends ServiceProvider
 {
     public function boot(){
+        if($this->app->runningInConsole()){
+            $this->registerPublishing();
+        }
+
         $this->registerResources();
+        $this->registerRoutes();
 
     }
 
@@ -22,5 +29,26 @@ class AcmeDoorPadBaseServiceProvider extends ServiceProvider
 
     private function registerResources(){
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+
+    protected function registerRoutes(){
+        Route::group($this->routeConfiguration(), function(){
+           $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+    }
+
+    private function routeConfiguration()
+    {
+        return [
+            'prefix' => config('acme_access.acme_access')==null ? 'acme_access' : config('acme_access.acme_access'),
+        ];
+    }
+
+    protected function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__.'/../config/acme_access.php' => config_path('acme_access.php')
+        ], 'acme-config');
     }
 }
