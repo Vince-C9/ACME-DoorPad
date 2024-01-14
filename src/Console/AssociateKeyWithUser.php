4740cf9638282ea1ffc068917cc3867ea9f4b997
vince <vincecole6@hotmail.co.uk>
2024-01-14 19:59:18 +0000

    New commands & config to ensure that they are registered.

--- src/Console/AssociateKeyWithUser.php
+++ src/Console/AssociateKeyWithUser.php
@@ -5,26 +5,38 @@ namespace Vince\AcmeDoorPad\Console;
 
 
 use Illuminate\Console\Command;
-use Vince\AcmeDoorPad\Jobs\QueueAdditionalKeys;
-use Vince\AcmeDoorPad\Services\KeyCodes\KeyCodeService;
+use PHPUnit\Event\Code\Throwable;
+use Vince\AcmeDoorPad\Models\Key;
+use Vince\AcmeDoorPad\Models\KeypadUser;
 
-class SecurityKeyCommand extends Command
+class AssociateKeyWithUser extends Command
 {
-    protected $signature = "security:keyAdd {--amount=1}";
-    protected $description = "Adds a security key to the database.";
+    protected $signature = "security:associate {user_id} {key}";
+    protected $description = "Associates the provided key with the provided user.  Example: security:associate USER_ID KEY_ID";
 
     public function handle(){
-        $keyService = new KeyCodeService();
-        $amount = $this->option('amount') > 0 ? $this->option('amount') : 1;
-        $keys=[];
-        for($i=0; $i < $amount; $i++ ){
-            $keys[] = $keyService->generateUniqueKey();
-        }
+        $key = $this->argument('key');
+        $user = $this->argument('user_id');
 
-        $keys=array_chunk($keys, 100, true);
-        foreach($keys as $batch){
-            QueueAdditionalKeys::dispatch($batch);
-        }
+        try{
+            if(!KeypadUser::hasKeyAssigned($user)){
+                if(Key::exists($key)){
+                    Key::assignKey($user, $key);
+                    $this->info("Command Complete. See below");
+                    $u=KeypadUser::find($user);
+                    $this->info("The user ".$u->name." ".$u->surname." has been associated with the key: ".$u->key->key);
+                }else{
+                    $this->info("This key doesn't exist in the database.");
+                }
 
+            }else{
+                $this->info("User already has a key assigned to them.");
+            }
+            return 0;
+        }catch(Throwable $t){
+            report($t);
+            $this->info($t->getMessage());
+            return 1;
+        }
     }
 }
