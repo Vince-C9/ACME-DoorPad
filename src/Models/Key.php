5f1deb7d6ff1e3ea08e28f30435a549fac06ce33
vince <vincecole6@hotmail.co.uk>
2024-01-14 18:27:38 +0000

    Write methods to support the test logic using raw SQL to ensure quick responses.

--- src/Models/Key.php
+++ src/Models/Key.php
@@ -6,6 +6,8 @@ namespace Vince\AcmeDoorPad\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Vince\AcmeDoorPad\Services\KeyCodes\KeyCodeService;
 
 class Key extends Model
 {
@@ -15,7 +17,53 @@ class Key extends Model
     ];
 
 
+    /**
+     * Set up relationship.
+     * @return BelongsTo
+     */
     public function keypadUser(): BelongsTo{
         return $this->belongsTo(KeypadUser::class);
+    }
+
+
+    /**
+     * Assign user a the provided key as long as that key isn't already allocated.
+     * Needs better error handling, but time!
+     *
+     * @param int $user_id
+     * @param int $key
+     * @param bool $new
+     */
+    public function assignKey(int $user_id, int $key){
+
+        $sql = "UPDATE keys SET keypad_user_id = ? WHERE keypad_user_id IS NULL AND key = ?";
+
+        try{
+            $result = DB::Update($sql, [$user_id, $key]);
+            return true;
+        }catch(\Throwable $t){
+            report($t->getMessage());
+        }
+        return false;
+    }
+
+
+    /**
+     * Strip the provided users key from them.
+     * Needs better error handling, but time!
+     *
+     * @param $user_id
+     * @return bool
+     */
+    public function stripKey(int $user_id){
+        $sql = "UPDATE keys SET keypad_user_id = NULL WHERE keypad_user_id = ?";
+
+        try{
+            $result = DB::Update($sql, [$user_id]);
+            return true;
+        }catch(\Throwable $t){
+            report($t->getMessage());
+        }
+        return false;
     }
 }
--- src/Models/KeypadUser.php
+++ src/Models/KeypadUser.php
@@ -6,6 +6,7 @@ namespace Vince\AcmeDoorPad\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\DB;
 
 class KeypadUser extends Model
 {
@@ -22,4 +23,12 @@ class KeypadUser extends Model
         return $this->hasOne(Key::class);
     }
 
+    public function hasKeyAssigned($user_id){
+        //Raw SQL as the keys could be very numerous - we want to keep this rapid.
+        $userKeySQL = "SELECT id FROM keys WHERE keypad_user_id = ?";
+        $result = DB::Select($userKeySQL, [$user_id]);
+
+        //!=0 instead of 1 in case we move away from single keys
+        return count($result)!==0;
+    }
 }
